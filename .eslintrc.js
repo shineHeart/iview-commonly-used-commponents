63282b18d7233d0eb0e19f518551c8ae013477e4
zsy <shangyou.zhang@xiaoyangedu.com>
2020-06-07 11:59:09 +0800

    重置eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
-    jquery: true
+    node: true
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
-  rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }, { 'SwitchCase': 2 }],
-    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
-  },
   parserOptions: {
     parser: 'babel-eslint'
   },
-  overrides: [
-    {
-      'files': ['*.vue'],
-      'rules': {
-        'indent': 'off'
-      }
-    }
-  ]
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 }
